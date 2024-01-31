d442da3dc31c5fd326372a090694be1113280923
Lonten Technology <142191915+LontenTechnology@users.noreply.github.com>
2024-01-31 09:20:14 +0800

    Add files via upload

---
+++ Arduino_Code/Project_4_Digtal Inputs/Project_4_Button_Controlled_LED/Project_4_Button_Controlled_LED.ino
@@ -0,0 +1,25 @@
+int Redled=11;// initialize Redled 11
+int inpin=7;// initialize pin 7
+boolean change=false;// define val
+void setup()
+{
+    pinMode(Redled,OUTPUT);// set LED pin as “output”
+    pinMode(inpin,INPUT);// set button pin as “input”
+}
+void loop()
+{
+  while(digitalRead(inpin)==HIGH)
+  {
+    delay(500);
+    if(change==true)
+    {
+      digitalWrite(Redled,LOW);
+      change=!change;
+    }
+    else
+    {
+      digitalWrite(Redled,HIGH);
+      change=!change;
+    }
+  }
+}
